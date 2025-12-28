ec21e7c7f59b905bff87601fdeb9bbbbf1147404
Bishesh Tiwaree <bisheshtiwaree@live.com>
2025-12-28 17:22:27 +0100

    chore(middleware): set runtime to 'nodejs' in middleware configuration

--- apps/web-app/middleware.ts
+++ apps/web-app/middleware.ts
@@ -7,4 +7,5 @@ export const config = {
     // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
+  runtime: 'nodejs',
 };
